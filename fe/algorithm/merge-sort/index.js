b6381c36ee5381db290c6105d11acf24bb452c2f
liub <liub@docimax.com.cn>
2020-03-10 15:36:07 +0800

    feature/mergeSort: practice merge sort.

--- fe/algorithm/merge-sort/index.js
+++ fe/algorithm/merge-sort/index.js
@@ -1,13 +1,38 @@
-const arr = [3, 22, 51, 10, 89, 12, 43, 3, 521, 32, 66, 42, 73, 17, 45, 66, 0, 89, 99, 135]
-
 /**
  * merge sort
  *
  * 假设要排序第 x 到 y 下标之间的元素，那么
  *
  * 递推公式：
- * mergeSort(x, y) = merge(mergeSort(x, (y - x) / 2 + 1), mergeSort((y - x) / 2 + 1, y))
+ * mergeSort(x, y) = merge(mergeSort(x, x + (y - x) / 2), mergeSort(x + (y - x) / 2 + 1, y))
  *
  * 终止条件：
  * x >= y (意思是不能再分了)
  */
+
+const arr = [3, 22, 66, 0, 99, 89, 1]
+
+function sort (startIdx, endIdx) {
+  if (startIdx >= endIdx) return
+
+  let splitIdx = Number.parseInt((endIdx - startIdx) / 2) + startIdx
+  sort(startIdx, splitIdx)
+  sort(splitIdx + 1, endIdx)
+
+  rank(startIdx, splitIdx, endIdx)
+}
+
+function rank (startIdx, splitIdx, endIdx) {
+  let temp = [], leftIdx = startIdx, rightIdx = splitIdx + 1
+
+  while (leftIdx <= splitIdx && rightIdx <= endIdx)
+    temp.push(arr[leftIdx] > arr[rightIdx] ? arr[rightIdx++] : arr[leftIdx++])
+
+  while (leftIdx <= splitIdx) temp.push(arr[leftIdx++])
+
+  while (rightIdx <= endIdx) temp.push(arr[rightIdx++])
+
+  temp.forEach((item, idx) => arr[startIdx + idx] = item)
+}
+
+sort(0, arr.length - 1)
---
+++ fe/algorithm/quick-sort/readme.md
@@ -0,0 +1,11 @@
+# Quick Sort 快排
+
+快排和归并排序非常相似，都是采用分治+递归的思路来解决问题
+
+```js
+递推公式：
+quickSort(x, y) = quickSort(x, x + (y - x) / 2) + quickSort(x + (y - x) / 2 + 1, y)
+
+终止条件：
+x >= y
+```
