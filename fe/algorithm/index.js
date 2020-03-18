4f3b3dedc7f1533bf4246697f49ee6f6ef97d6e9
bei <bey.liu@outlook.com>
2020-03-19 07:19:48 +0800

    最好、最坏、平均时间复杂度分析

--- fe/algorithm/index.js
+++ fe/algorithm/index.js
@@ -1,6 +1,10 @@
-function foo (n) {
-  let sum = 0
-  for (let i = 0; i < n; i++) {
-    sum += i
+let array = [6, 3, 2, 10, 32, 5, 0, 12, 1]
+
+function search (target) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) return i
   }
+  return -1
 }
+
+console.log(search(6));
